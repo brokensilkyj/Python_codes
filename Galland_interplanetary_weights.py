0e073a06da2c03a277153825c054d2e6945c5f6d
brokensilkyj <43531567+brokensilkyj@users.noreply.github.com>
2018-09-24 14:12:24 -0400

    Add files via upload

---
+++ Galland_interplanetary_weights.py
@@ -0,0 +1,25 @@
+# Dylan Galland
+# Planetary Weights assignment
+
+# Enter your weight as a float
+flWeight = float(input("Enter your weight: "))
+
+# The conversion weight for the planets
+Mercury = flWeight * 0.38
+Venus = flWeight * 0.91
+Moon = flWeight * 0.165
+Mars = flWeight * 0.38
+Jupiter = flWeight * 2.34
+Saturn = flWeight * 0.93
+Uranus = flWeight * 0.92
+Pluto = flWeight * 0.066
+
+# The results of your weight on each planet
+print ("Your weight on the planet Mercury is:", format( Mercury, "0.2f"))
+print ("Your weight on the planet Venus is:", format( Venus, "0.2f"))
+print ("Your weight on the planet the Moon is:", format( Moon, "0.2f"))
+print ("Your weight on the planet Mars is:", format( Mars, "0.2f"))
+print ("Your weight on the planet Jupiter is:", format( Jupiter, "0.2f"))
+print ("Your weight on the planet Saturn is:", format( Saturn, "0.2f"))
+print ("Your weight on the planet Uranus is:", format( Uranus, "0.2f"))
+print ("Your weight on the planet Pluto is:", format( Pluto, "0.2f"))
